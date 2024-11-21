842fbcd837a539a053d4dbb2f891d74c93eb52c0
Simon Zeni <simon.zeni@collabora.com>
2024-11-21 11:43:23 -0500

    xrt: adapt driver to new get_view_poses api change

--- src/xrt/auxiliary/util/u_device.c
+++ src/xrt/auxiliary/util/u_device.c
@@ -449,7 +449,7 @@ u_device_get_view_pose(const struct xrt_vec3 *eye_relation, uint32_t view_index,
  *
  */
 
-void
+xrt_result_t
 u_device_get_view_poses(struct xrt_device *xdev,
                         const struct xrt_vec3 *default_eye_relation,
                         int64_t at_timestamp_ns,
@@ -458,7 +458,11 @@ u_device_get_view_poses(struct xrt_device *xdev,
                         struct xrt_fov *out_fovs,
                         struct xrt_pose *out_poses)
 {
-	xrt_device_get_tracked_pose(xdev, XRT_INPUT_GENERIC_HEAD_POSE, at_timestamp_ns, out_head_relation);
+	xrt_result_t xret =
+	    xrt_device_get_tracked_pose(xdev, XRT_INPUT_GENERIC_HEAD_POSE, at_timestamp_ns, out_head_relation);
+	if (xret != XRT_SUCCESS) {
+		return xret;
+	}
 
 	for (uint32_t i = 0; i < view_count && i < ARRAY_SIZE(xdev->hmd->views); i++) {
 		out_fovs[i] = xdev->hmd->distortion.fov[i];
@@ -467,6 +471,8 @@ u_device_get_view_poses(struct xrt_device *xdev,
 	for (uint32_t i = 0; i < view_count; i++) {
 		u_device_get_view_pose(default_eye_relation, i, &out_poses[i]);
 	}
+
+	return XRT_SUCCESS;
 }
 
 
@@ -508,7 +514,7 @@ u_device_ni_set_output(struct xrt_device *xdev, enum xrt_output_name name, const
 	E(get_hand_tracking);
 }
 
-void
+xrt_result_t
 u_device_ni_get_view_poses(struct xrt_device *xdev,
                            const struct xrt_vec3 *default_eye_relation,
                            int64_t at_timestamp_ns,
@@ -517,7 +523,8 @@ u_device_ni_get_view_poses(struct xrt_device *xdev,
                            struct xrt_fov *out_fovs,
                            struct xrt_pose *out_poses)
 {
-	E(get_hand_tracking);
+	E(get_view_poses);
+	return XRT_ERROR_NOT_IMPLEMENTED;
 }
 
 bool
