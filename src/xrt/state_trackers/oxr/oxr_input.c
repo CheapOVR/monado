119e00703bf2038890f7612547ca67abe4513b13
Christoph Haag <christoph.haag@collabora.com>
2025-03-24 03:17:48 +0100

    st/oxr: Fix typo in oxr_binding_find_bindings_from_key signature

--- src/xrt/state_trackers/oxr/oxr_input.c
+++ src/xrt/state_trackers/oxr/oxr_input.c
@@ -666,6 +666,8 @@ get_matched_xrpath(struct oxr_binding *b, const struct oxr_action_ref *act)
 	return preferred_path;
 }
 
+
+
 static void
 get_binding(struct oxr_logger *log,
             struct oxr_sink_logger *slog,
@@ -739,7 +741,7 @@ get_binding(struct oxr_logger *log,
 	    log,                            // log
 	    profile,                        // p
 	    act_ref->act_key,               // key
-	    ARRAY_SIZE(binding_points),     // max_bounding_count
+	    ARRAY_SIZE(binding_points),     // max_binding_count
 	    binding_points,                 // out_bindings
 	    &binding_count);                // out_binding_count
 	if (binding_count == 0) {
